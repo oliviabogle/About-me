f3e4c50d1e9a7074e3fa76c6dc798bfe536bc7e7
Olivia Bogle <oliviabogle1@outlook.com>
2025-10-30 23:28:19 -0500

    Link Pages

--- contactMe/scripts/form.js
+++ contactMe/scripts/form.js
@@ -1,79 +1,3 @@
-/* // Get data 
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const messageInput = document.querySelector("#message");
-const success = document.querySelector("#success");
-const errorNodes = document.querySelectorAll(".error");
-
-// Validate
-function validateForm(event){
-    event.preventDefault();
-    clearMessages();
-    let errorFlag = false;
-
-    if(nameInput.value.length < 1){
-        errorNodes[0].innerText = "Name cannot be blank";
-        nameInput.classList.add("error-border");
-        errorFlag = true;
-    }
- 
-    if(!emailIsValid(emailInput.value)){
-        errorNodes[1].innerText = "Invalid email";
-        emailInput.classList.add("error-border");
-        errorFlag = true;
-    }
-
-    if(messageInput.value.length < 1){
-        errorNodes[2].innerText = "Please enter a message";
-        messageInput.classList.add("error-border");
-        errorFlag = true;
-    }
-
-    if(!errorFlag){
-        sendEmail();
-        //success.innerText = "Success!";
-    }
-} 
-
-// Send email using EmailJS
-function sendEmail() {
-  const params = {
-    name: nameInput.value,
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-
-  emailjs
-    .send("service_ymhxh2l", "template_otyz6tg", params)
-    .then(() => {
-      success.innerText = "Message sent successfully!";
-    })
-    .catch((error) => {
-      success.innerText = "Failed to send message. Try again later.";
-      console.error("EmailJS error:", error);
-    });
-}
-
-// Clear error messages
-function clearMessages(){
-    for(let i=0; i < errorNodes.length; i++){
-        errorNodes[i].innerText = "";
-    }
-    success.innerText = "";
-    nameInput.classList.remove("error-border");
-    emailInput.classList.remove("error-border");
-    messageInput.classList.remove("error-border");
-}
-
-// Is email valid?
-function emailIsValid(email){
-    let pattern = /\S+@\S+\.\S+/;
-    return pattern.test(email);
-} 
-
-document.querySelector("form").addEventListener("submit", validateForm);
-*/
-
 // Get data
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
@@ -122,7 +46,7 @@ function sendEmail() {
     .send("service_ymhxh2l", "template_otyz6tg", params)
     .then(() => {
       success.innerText = "Message sent successfully!";
-      success.style.color = "#0f0";
+      success.style.color = "black";
       nameInput.value = "";
       emailInput.value = "";
       messageInput.value = "";
